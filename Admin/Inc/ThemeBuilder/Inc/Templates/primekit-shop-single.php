25af499c5107cb86edbb20c03b2f0e3a18e54b82
hmbashar <hmbashar@gmail.com>
2025-04-07 13:47:08 +0600

    shop tempalate markup update

--- Admin/Inc/ThemeBuilder/Inc/Templates/primekit-shop-single.php
+++ Admin/Inc/ThemeBuilder/Inc/Templates/primekit-shop-single.php
@@ -1,8 +1,35 @@
 <?php
-use Elementor\Plugin as ElementorPlugin;
+/*
+ * WooCommerce Single Product Template
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
+
+\Elementor\Plugin::$instance->frontend->add_body_class( 'elementor-template-full-width' );
 
 get_header();
-echo ElementorPlugin::instance()->frontend->get_builder_content_for_display(
-    \PrimeKit\Admin\Inc\ThemeBuilder\ThemeBuilder::get_template_id('shop_single')
-);
+
+/**
+ * Before Elementor Header-Footer content.
+ */
+do_action( 'elementor/page_templates/header-footer/before_content' );
+
+echo '<main class="primekit-single-product">';
+
+if ( ! \Elementor\Plugin::$instance->preview->is_preview_mode() ) {
+    do_action( 'primekit_shop_single_content' );
+} else {
+    echo '<div class="primekit-container">';
+    wc_get_template_part( 'content', 'single-product' );
+    echo '</div>';
+}
+
+echo '</main>';
+
+/**
+ * After Elementor Header-Footer content.
+ */
+do_action( 'elementor/page_templates/header-footer/after_content' );
+
 get_footer();
