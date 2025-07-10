8121b248fd9ad23ed521dc50cdcc893a6bca597a
[Codedbyabir] <[abir.supreox@gmail.com]>
2025-07-10 13:24:07 +0600

    Add URL support to Advanced List widget

--- Frontend/Elementor/Widgets/AdvancedList/RenderView.php
+++ Frontend/Elementor/Widgets/AdvancedList/RenderView.php
@@ -1,19 +1,19 @@
 <?php
 if (!defined('ABSPATH'))
     exit; // Exit if accessed directly
-
 use Elementor\Icons_Manager;
-
 $settings = $this->get_settings_for_display();
-$count = 1;
 
+$count = 1;
 if (!empty($settings['primekit_advanced_list_items'])) {
     echo '<ul class="primekit-advanced-list-wrapper">';
     foreach ($settings['primekit_advanced_list_items'] as $item) {
+
         echo '<li class="primekit-advanced-list-item">';
 
-        $has_link = !empty($item['list_item_url']['url']);
-        if ($has_link) {
+        $anchor_url = isset($item['list_item_url']['url']) ? $item['list_item_url']['url'] : '';
+        // Check if the item has a URL
+        if (!empty($anchor_url)) {
             $target = !empty($item['list_item_url']['is_external']) ? ' target="_blank"' : '';
             $nofollow = !empty($item['list_item_url']['nofollow']) ? ' rel="nofollow"' : '';
             echo '<a href="' . esc_url($item['list_item_url']['url']) . '" class="primekit-advanced-list-item-link"' . $target . $nofollow . '>';
@@ -25,21 +25,18 @@ if (!empty($settings['primekit_advanced_list_items'])) {
             Icons_Manager::render_icon($item['list_assets_icon'], ['aria-hidden' => 'true']);
             echo '</div>';
         }
-
         // Image
         if ($item['list_assets_type'] == 'image' && !empty($item['list_assets_img']['url'])) {
             echo '<div class="primekit-advanced-list-item-img">';
             echo '<img src="' . esc_url($item['list_assets_img']['url']) . '" alt="">';
             echo '</div>';
         }
-
         // Count
         if ($item['list_assets_type'] == 'count') {
             echo '<div class="primekit-advanced-list-item-count">';
             echo '<span>' . esc_html($count) . '</span>';
             echo '</div>';
         }
-
         echo '<div class="primekit-advanced-list-item-content">';
         if (!empty($item['list_title'])) {
             echo '<h4 class="primekit-advanced-list-title">' . esc_html($item['list_title']) . '</h4>';
@@ -48,11 +45,9 @@ if (!empty($settings['primekit_advanced_list_items'])) {
             echo '<p class="primekit-advanced-list-text">' . esc_html($item['list_sub_title']) . '</p>';
         }
         echo '</div>';
-
-        if ($has_link) {
+        if (!empty($anchor_url)) {
             echo '</a>';
         }
-
         echo '</li>';
         $count++;
     }
